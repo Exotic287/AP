23d67e773ed53ec71df43b62b4a1e93ffd368a4d
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 12:45:26 +0700

    Program Tujuh
    Membuat Segitiga Sama Kaki
    By.Rzl

---
+++ PemprogramanAlgoritma/src/Pertemuan07/LatihanC.java
@@ -0,0 +1,18 @@
+
+package Pertemuan07;
+
+public class LatihanC {
+    public static void main(String[] args) {
+                    int a=5;
+            for (int i=1;i<=a;i++){
+                for(int c=0;c<=(a-i);c++){
+                    System.out.print(" ");
+                }
+                for (int c=0;c<=i-1;c++){
+                    System.out.print(i+" ");
+                }
+                System.out.println("");
+            }
+    }
+    
+}
