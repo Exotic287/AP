410d434b3d3a3bda544bf55f328b84efcd3a229a
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 12:48:58 +0700

    Progam Tujuh
    Membuat Segitiga Sama Kaki (Terbalik)
    By.Rzl

---
+++ PemprogramanAlgoritma/src/Pertemuan07/LatihanD.java
@@ -0,0 +1,18 @@
+
+package Pertemuan07;
+
+public class LatihanD {
+    public static void main(String[] args) {
+        int i,a,b;
+        for (i=5;i>=1;i--){
+            for (a=5;a>=i;a--){
+                System.out.print(" ");
+            }
+            for (b=1;b<i+1;b++){
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
