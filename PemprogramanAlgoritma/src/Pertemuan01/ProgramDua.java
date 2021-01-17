d2d3d6a48c72c2bc5bb19cc2d9f1928409c91117
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 11:33:59 +0700

    Create ProgramDua.java

---
+++ PemprogramanAlgoritma/src/Pertemuan01/ProgramDua.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan01;
+
+/**
+ *
+ * @author Exotic
+ */
+public class ProgramDua {
+    public static void main(String[] args) {
+        System.out.println("===========================");
+        System.out.println("         DATA DIRI         ");
+        System.out.println("===========================");
+        System.out.println("   NIM  : 20.240.0101"   );
+        System.out.println("   NAMA : RIZAL ARDIANTO");
+        System.out.println("   KELAS:P41");
+        System.out.println("===========================");
+        System.out.println("     STMIK WIDYA PRATAMA   ");
+        System.out.println("===========================");
+    }
+    
+}
