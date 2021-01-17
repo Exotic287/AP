0ea608d1cb90e78a0576213b8fb439d9dfb52b24
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 12:43:47 +0700

    Program Tujuh
    Membuat Segitiga Siku Siku
    By.Rizal

---
+++ PemprogramanAlgoritma/src/Pertemuan07/LatihanB.java
@@ -0,0 +1,17 @@
+
+package Pertemuan07;
+
+public class LatihanB {
+    public static void main(String[] args) {
+            for (int i=1;i<=5;i++){
+            for (int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
