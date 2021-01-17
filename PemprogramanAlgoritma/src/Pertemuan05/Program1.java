39dbff8715f4dd2ff7a601575ba82b754d8bb67a
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 12:21:56 +0700

    Program Lima
    By.Rizal

---
+++ PemprogramanAlgoritma/src/Pertemuan05/Program1.java
@@ -0,0 +1,11 @@
+
+package Pertemuan05;
+
+public class Program1 {
+    public static void main(String[] args) {
+        for (int i=0;i<=5;i++){
+            System.out.println("STMIK Widya Pratama");
+        }
+    }
+    
+}
