0837b9eecc811f330cff58fb981cc6763ca5a843
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 11:38:27 +0700

    Pertemuan Dua
    Matkul Algoritma Pemprogaman

---
+++ PemprogramanAlgoritma/src/Pertemuan02/ProgramSatu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan02;
+
+/**
+ *
+ * @author Exotic
+ */
+public class ProgramSatu {
+    public static void main(String[] args) {
+        //membuat variabel (deklarasi variabel)
+        String nama, alamat;
+        int umur;
+        double tinggiBadan;
+        //mengisi variabel
+        nama = "Rizal Ardianto";
+        alamat = "Sragi";
+        umur = 18;
+        tinggiBadan = 172.5;
+        //menampilkan data pada variabel
+        System.out.println ("============DATA DIRI===========");
+        System.out.println ("Nama           = "+nama);
+        System.out.println ("Alamat         = "+alamat);
+        System.out.println ("Umur           = "+umur);
+        System.out.println ("Tinggi Badan   = "+tinggiBadan);
+    }
+    
+}
