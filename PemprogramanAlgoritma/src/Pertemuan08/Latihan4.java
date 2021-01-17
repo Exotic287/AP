8fd6377f5e6883891798172a9a52310d3e9c257e
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 13:09:38 +0700

    Program Delapan
    By.Rzl

---
+++ PemprogramanAlgoritma/src/Pertemuan08/Latihan4.java
@@ -0,0 +1,22 @@
+
+package Pertemuan08;
+
+import javax.swing.JOptionPane;
+
+public class Latihan4 {
+    public static void main(String[] args) {
+        int pilihan = JOptionPane.showConfirmDialog(null,"Silahkan Konfirmasi?");
+        switch(pilihan){
+            case JOptionPane.YES_OPTION:
+                JOptionPane.showMessageDialog(null, "Kamu memilih YES");
+                break;
+            case JOptionPane.NO_OPTION:
+                JOptionPane.showMessageDialog(null, "Kamu memilih No");
+                break;
+            case JOptionPane.CANCEL_OPTION:
+                JOptionPane.showMessageDialog(null, "Kamu Memilh CANCEL");
+                break;
+        }
+    }
+    
+}
