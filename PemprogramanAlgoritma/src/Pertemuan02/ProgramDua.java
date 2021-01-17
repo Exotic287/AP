beb2e286f4edf9673c0882d4f6f73189b2a48fb4
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 11:44:47 +0700

    Pertemuan Dua

---
+++ PemprogramanAlgoritma/src/Pertemuan02/ProgramDua.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan02;
+
+/**
+ *
+ * @author Exotic
+ */
+public class ProgramDua {
+    public static void main(String[] args) {
+        double phi, r, luasLingkaran;
+        
+        //input nilai variabel
+        phi = 3.14;
+        r = 7;
+        //proses
+        luasLingkaran = phi*r*r;
+        //output
+        System.out.print ("Luas Lingkaran Dengan Jari-Jari "+r+" = ");
+        System.out.println(luasLingkaran);
+    }
+    
+}
