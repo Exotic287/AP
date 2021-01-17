0d0b28af1c9f2ea10c2f71d5695e59513e895c7f
Exotic287 <77543983+Exotic287@users.noreply.github.com>
2021-01-17 13:00:24 +0700

    Program Delapan
    By.Rzl

---
+++ PemprogramanAlgoritma/src/Pertemuan08/Latihan3.java
@@ -0,0 +1,12 @@
+
+package Pertemuan08;
+
+import javax.swing.JOptionPane;
+
+public class Latihan3 {
+    public static void main(String[] args) {
+        String nama = JOptionPane.showInputDialog("Masukan Nama : ");
+        JOptionPane.showMessageDialog(null, "Nama Kamu : "+ nama);
+    }
+    
+}
